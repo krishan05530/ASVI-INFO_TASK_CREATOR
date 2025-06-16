d10abde2a131e65f6fda730201c4011e632df687
krishan kumar <kumarkrishan05530@gmail.com>
2025-06-16 22:57:36 +0530

    Fix: cleaned up ignored files

--- server/index.js
+++ server/index.js
@@ -9,12 +9,32 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 
+// app.use(cors({
+//     origin: process.env.BASE_URL,
+//     credentials:true,
+//     methods:['GET','POST','PUT','DELETE','OPTIONS'],
+//     allowedHeaders:['Content-Type','Authorization']
+// }))
+
+
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://asvi-info-task-creator-e6x3.vercel.app'
+];
+
 app.use(cors({
-    origin: process.env.BASE_URL,
-    credentials:true,
-    methods:['GET','POST','PUT','DELETE','OPTIONS'],
-    allowedHeaders:['Content-Type','Authorization']
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use("/tasks",taskRoutes);
 const port= process.env.PORT ||4000;
