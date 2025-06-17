c8b9897a2ba929307afdbeeac22df689edf37058
krishan kumar <kumarkrishan05530@gmail.com>
2025-06-17 09:40:26 +0530

    chnanges in cors

--- server/index.js
+++ server/index.js
@@ -49,20 +49,29 @@ app.use(express.urlencoded({extended:true}))
 // }));
 
 
+// app.use(cors({
+//     origin: [
+//         'https://asvi-info-task-creator-e6x3-3t4tg1xlp.vercel.app',
+//   'https://asvi-info-task-creator-e6x3.vercel.app',
+//   'https://asvi-info-task-crea-git-e04fda-krishan-kumars-projects-91e715cc.vercel.app'
+//     ],
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
 app.use(cors({
     origin: [
         'https://asvi-info-task-creator-e6x3-3t4tg1xlp.vercel.app',
-  'https://asvi-info-task-creator-e6x3.vercel.app',
-  'https://asvi-info-task-crea-git-e04fda-krishan-kumars-projects-91e715cc.vercel.app'
+        'https://asvi-info-task-creator-e6x3.vercel.app',
+        'https://asvi-info-task-crea-git-e04fda-krishan-kumars-projects-91e715cc.vercel.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-
-
-
+app.options("*", cors());
 app.use("/tasks",taskRoutes);
 const port= process.env.PORT ||4000;
 
@@ -71,7 +80,7 @@ app.get("/",(req,res)=>{
 })
 
 
-app.listen(process.env.PORT,()=>{
+app.listen(port,()=>{
     console.log(`listing on port  ${port}`);
 })
 
